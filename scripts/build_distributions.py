631b8d711c970dabe5cfce47f3def1c8dec0f461
Stephen French <stephenfrench9@gmail.com>
2018-01-17 15:38:19 -0800

    replace function "keep" with "article_text", rewrite function "clean"

--- scripts/build_distributions.py
+++ scripts/build_distributions.py
@@ -69,9 +69,6 @@ def addAffordance(thing, affordance):
         all_affordances[thing][affordance] = 1
 
 
-# input: "thisString" is text to be modified. "options" is
-# regular expression to locate text to be replaced.
-# Returns: a modified string.
 def optionsSub(thisString, options):
     """ Remove and replace text in a wikicode string
 
@@ -79,6 +76,8 @@ def optionsSub(thisString, options):
         thisString -- wikicode string to be modified
         options -- regex to locate parts of the string
         that look like [[...|...]]
+
+        Return a modified string
     """
     a = options.findall(thisString)
     for option in a:
@@ -94,84 +93,92 @@ def optionsSub(thisString, options):
     return thisString
 
 
-# input: cleans "thisString".
-# ouput: returns a cleaned version of "thisString"
 def clean(thisString):
-    # references
-    ref1 = re.compile("&lt;ref&gt;.*?&lt;/ref&gt;")
-    thisString = ref1.sub("", thisString)
-    ref2 = re.compile("&lt;ref.*?&gt;.*?&lt;/ref&gt;")
-    thisString = ref2.sub("", thisString)
-    hangingRef = re.compile("&lt;ref&gt;.*$")
-    thisString = hangingRef.sub("", thisString)
-    hangingRef2 = re.compile("&lt;ref.*?&gt;")
-    thisString = hangingRef2.sub("", thisString)
+    """Remove all wikimarkup text from a line
+       from a wikidump.
 
-    # everything else
-    quotes = re.compile("&....;")
+       Returns a string that can be read as natural text.
+
+       positional arguments:
+
+       thisString -- line from a wikidump.
+    """
+    regex_and_replacements = {
+
+        # "&lt;ref&gt;.*?&lt;/ref&gt;" : "", # ref
+
+        "&lt;ref.*?&gt;.*?&lt;/ref&gt;": "",  # same as ref?
+
+        "&lt;ref&gt;.*$": "",  # hangingref
+
+        "&lt;ref.*?&gt;": "",  # hangingref2
+
+        "&amp;nbsp;": "",  # unknown
+
+        "{{.*?}}": "",  # excessive citations
+
+        "&....;": "\"",  # quotes
+
+        # "{{lang.*?}}" : "foreign word", #maybe not needed in python 3 used
+
+        # "\â\€\“" : "-", # other EMdashes
+
+        # "\â\€\”.*?\â\€\”" : " ", # EMdashes
+
+    }
+
+    for regex_template in regex_and_replacements.keys():
+        regex = re.compile(regex_template)
+        thisString = regex.sub(regex_and_replacements[regex_template],
+                               thisString)
+
     options = re.compile("\[\[.*?\]\]")
-    funky = re.compile("&amp;nbsp;")
-    foreign_words = re.compile("{{lang.*?}}")  # maybe not needed if python 3
-    EMDashes = re.compile("\â\€\”.*?\â\€\”")
-    otherEMDashes = re.compile("\â\€\“")
-    excessiveCitations = re.compile("{{.*?}}")
-    # hangingCurly = re.compile("{{[^\.]*?$")
-    # exoticAlphabet = re.compile("{{lang.*?}}")
-
-    # make replacements
-    thisString = quotes.sub("\"", thisString, count=0)
     thisString = optionsSub(thisString, options)
-    thisString = funky.sub("", thisString)
-    thisString = foreign_words.sub("foreign word", thisString)
-    thisString = EMDashes.sub(" ", thisString)
-    thisString = otherEMDashes.sub("-", thisString)
-    thisString = excessiveCitations.sub("", thisString)
-    # thisString = hangingCurly.sub("SCAROFCURLY",thisString)
-    # thisString = exoticAlphabet.sub("foreign word",thisString)
+
     return(thisString)
 
 
-# Should we clean the line?
-def cleanable(line):
-    keep = True
+def successfully_cleaned(line):
+    """ Decide if the line has been successfully cleaned.
 
-    if(len(line) < 200):
-        keep = False
-    elif(line[0] == ' '):
-        keep = False
-    elif(line[0] == '&'):
-        keep = False
-    elif(line[0] == '<'):
-        keep = False
-    elif(line[0:2] == '{{'):
-        keep = False
-    elif(line[0:3] == '==='):
-        keep = False
-    elif(line[0:6] == '[[File'):
-        keep = False
-    elif(line[0] == '*'):
-        keep = False
-    elif(line[0] == '|'):
-        keep = False
-    elif('Ã' in line):
-        keep = False
-    elif('<' in line and '>' in line):
-        keep = False
-    elif('|url' in line):
-        keep = False
-    return(keep)
+        Return false if the line is not clean.
+    """
 
-
-# Should we keep the cleaned line?
-def keep(line):
-    like = True
+    cleaned = True
     if('&lt' in line):
-        like = False
-    elif (len(line) < 100):
-        like = False
+        cleaned = False
+    elif (len(line) < 150):
+        cleaned = False
     elif('{' in line):
-        like = False
-    return like
+        cleaned = False
+    return cleaned
+
+
+def article_text(some_text_from_wikidump):
+    """ Analyze a line from a wikidump file and decide if it
+        is text from the article body.
+
+        Return True if it is text.
+    """
+
+    is_text = True
+    first_char = line[0]
+    forbidden_first_char = [
+        " ",
+        "{",
+        "}",
+        "|",
+        "=",
+        "[",
+        "]",
+        "*",
+        '<'
+    ]
+    if len(line) < 10:
+        is_text = False
+    elif first_char in forbidden_first_char:
+        is_text = False
+    return(is_text)
 
 
 if __name__ == '__main__':
@@ -201,20 +208,15 @@ if __name__ == '__main__':
 
         try:
             line = f.readline()
-
-            if cleanable(line):
+            if line and article_text(line):
                 line = clean(line)
-                if keep(line):
-                    if(line[0] == ':'):
-                        line = line[1:]
-                    if(line[0] == ":"):
-                        line = line[1:]
-                    print(line)
+                if successfully_cleaned(line):
                     process_paragraph(line)
         except UnicodeDecodeError:  # occurs more often in python 3
+            print("WE HAVE A UNICODE DECODE ERROR")
             line = True
-        linesProcessed += 1
 
+        linesProcessed += 1
         if linesProcessed % 100000 == 0:
             print(linesProcessed)
             print(time.time() - prevTime)
@@ -223,12 +225,6 @@ if __name__ == '__main__':
     f.close()
     finishTime = time.time()
 
-    # print("what does my all_afforances dictionary look like?")
-    # for thing in all_affordances:
-    #     print(thing)
-    #     for affordance in all_affordances[thing]:
-    #         print("\t" + affordance + " : " +
-    #                                str(all_affordances[thing][affordance]))
     print("Number of things (affordance distributions): " +
           str(len(all_affordances)))
     print("The program took this long (seconds): " +
@@ -241,38 +237,8 @@ if __name__ == '__main__':
     with open('..\\..\\all_affordances.p', 'wb') as g:
         pickle.dump(all_affordances, g)
 
-
-# NOTES: I have had some trouble reading from the xml file.
-# It would work in python2, sometimes printing characters incorrectly.
-# It would not work in python 3. It would read and print
-# for while, then throw UnicodeDecodeError.
-# The decode error gets thrown when calling a print command.
-# If I catch the error, it will print
-# most lines successfully. (All?) Letters with accents are
-# printed incorrectly (no error thrown).
-
-# # SIMPLY READ.
-# linesProcessed = 0
-# for line in f:
-#     if linesProcessed == 1000:
-#         break
-#     print("FROM THE E:\ DRIVE and  there are " + str(linesProcessed)
-#  + " previous lines")
-#     print(line)
-#     print()
-#     linesProcessed += 1
-# #WORKS IN PYTHON2 - YES
-# #WORKS IN PYTHON3 - NO NO NO
-
-# #SIMPLY READ.
-# linesProcessed = 0
-# for line in f:
-#     if linesProcessed == 1000:
-#         break
-#     print("FROM THE E:\ DRIVE and  there are " +
-#                                   str(linesProcessed) + " previous lines")
-#     print(line.encode('utf-8'))
-#     print()
-#     linesProcessed += 1
-# #WORKS IN PYTHON2 - DONT KNOW OR CARE
-#   #WORKS IN PYTHON3 - NO NO NO
+# Note: You are handling references incorrectly. Some of them get split across
+# lines. It'd take probably an hour to fix it. You currently get rid of lines
+# that start mid referece but you keep lines that end mid references. You could
+# get rid of both. Or, keep both and let spacy handle some fragments, or, knit
+# the lines back together. Up to you.
